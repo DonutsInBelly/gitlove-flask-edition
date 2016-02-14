a69d2b25870d0fb90d1fbb9416453181cf621b68
Carlin <carlinau@gmail.com>
2016-02-14 00:02:06 -0500

    oauth pls

--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import render_template, url_for, redirect, g
 from flask.ext.github import GitHub
+#from __init__ import app
 from app import app,github
 
 @app.route('/', methods=['GET'])
@@ -8,5 +9,18 @@ def index():
     return render_template('index.html')
 
 @app.route('/authorize')
-def auth():
-    return github.authorize(scope='user,repo')
+def authorize():
+    return github.authorize()
+
+#@app.route('/github-callback')
+#@github.authorized_handler
+#def authorized(oauth_token):
+    #next_url = request.args.get('next')
+
+@app.route('/dashboard')
+def dashboard():
+    return render_template('dashboard.html')
+
+@app.route('/test')
+def test():
+    return render_template('test.html')
